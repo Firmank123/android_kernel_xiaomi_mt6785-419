349b0114c6b63131d2811f5aef6d8ebf60a05d31
Firmank123 <firmankediri123@gmail.com>
2026-03-29 21:22:04 +0700

    Revert "drivers: mediatek: pmic: Update driver"
    This reverts commit 392a2424c2249aa0a1cc4949511fdf14724b0b1e.

--- drivers/misc/mediatek/include/mt-plat/v1/charger_class.h
+++ drivers/misc/mediatek/include/mt-plat/v1/charger_class.h
@@ -135,7 +135,6 @@ struct charger_ops {
 
 	/* charger type detection */
 	int (*enable_chg_type_det)(struct charger_device *dev, bool en);
-	int (*rerun_apsd)(struct charger_device *dev, bool en);
 
 	/* HVDCP type detection */
 	int (*get_hvdcp_type)(struct charger_device *dev, u32 *type);
@@ -272,8 +271,6 @@ extern int charger_dev_enable_hvdcp_det(
 extern int charger_dev_check_hv_charging(
 	struct charger_device *charger_dev);
 extern int charger_dev_enable_chg_type_det(
-	struct charger_device *charger_dev, bool en);
-extern int charger_dev_rerun_apsd(
 	struct charger_device *charger_dev, bool en);
 extern int charger_dev_enable_otg(
 	struct charger_device *charger_dev, bool en);
